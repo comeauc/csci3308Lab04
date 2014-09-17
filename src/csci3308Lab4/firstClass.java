bf0eb99098821fc447fef7d01e86189264c6cd96
comeauc <cameron.comeau@colorado.edu>
2014-09-17 11:26:02 -0600

    committing after the super annoying fetch setup

--- src/csci3308Lab4/firstClass.java
+++ src/csci3308Lab4/firstClass.java
@@ -3,5 +3,6 @@ package csci3308Lab4;
 public class firstClass {
 	public static void main(String[] args) {
 		System.out.println("hello world"); // display the string
+		System.out.println("how are you?"); // display the string
 	}
 }
