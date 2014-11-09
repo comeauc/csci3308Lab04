d324df038a160994b711cb8ed6fbedc9d9a08b54
cameron comeau <cameron.comeau@colorado.edu>
2014-11-09 14:40:46 -0700

    Revert "committing after the super annoying fetch setup"
    This reverts commit bf0eb99098821fc447fef7d01e86189264c6cd96.

--- src/csci3308Lab4/firstClass.java
+++ src/csci3308Lab4/firstClass.java
@@ -3,6 +3,5 @@ package csci3308Lab4;
 public class firstClass {
 	public static void main(String[] args) {
 		System.out.println("hello world"); // display the string
-		System.out.println("how are you?"); // display the string
 	}
 }
